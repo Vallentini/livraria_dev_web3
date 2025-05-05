29a1dc4088a528b1e77d551079b7b3832c9da857
vallentini <vallentinigabrielli.silva@gmail.com>
2025-05-05 16:42:14 -0300

    feat: criação de múltiplos serializadores para Livro.

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,11 +1,5 @@
 from .user import UserSerializer
-
 from .categoria import CategoriaSerializer
-
 from .editora import EditoraSerializer
-
 from .autor import AutorSerializer
-
-from .livro import LivroSerializer
-
-from .livro import LivroListRetrieveSerializer, LivroSerializer
+from .livro import LivroListSerializer, LivroRetrieveSerializer , LivroSerializer
