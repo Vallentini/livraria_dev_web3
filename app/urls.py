b8d08dbef8266a38eaed54d9f0ef5f607f439faa
vallentini <vallentinigabrielli.silva@gmail.com>
2025-05-20 15:20:32 -0300

    feat: inclusão da app de upload e associação de imagens.

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
@@ -9,13 +12,14 @@ from rest_framework.routers import DefaultRouter
 
 from core.views import UserViewSet, CategoriaViewSet, EditoraViewSet, AutorViewSet, LivroViewSet
 
+from uploader.router import router as uploader_router
+
 router = DefaultRouter()
 
 router.register(r'editora', EditoraViewSet)
 router.register(r'categorias', CategoriaViewSet)
 router.register(r'usuarios', UserViewSet, basename='usuarios')
 router.register(r'autor', AutorViewSet)
-router.register(r'livro', LivroViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -33,4 +37,7 @@ urlpatterns = [
     ),
     # API
     path('api/', include(router.urls)),
+    path("api/media/", include(uploader_router.urls)),
 ]
+
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
--- core/models/livro.py
+++ core/models/livro.py
@@ -3,6 +3,7 @@ from django.db import models
 from .categoria import Categoria
 from .autor import Autor
 from .editora import Editora
+from uploader.models import Image
 
 class Livro(models.Model):
     titulo = models.CharField(max_length=255)
@@ -16,6 +17,14 @@ class Livro(models.Model):
         Editora, on_delete=models.PROTECT, related_name="livros", null=True, blank=True
     )
     autores = models.ManyToManyField(Autor, related_name="livros", blank=True)
+    capa = models.ForeignKey(
+        Image,
+        related_name="+",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        default=None,
+    )
 
     def __str__(self):
         return f"({self.id}) {self.titulo} ({self.quantidade})"
