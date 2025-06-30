c9b59798a85508c5b9941d523add4599e57d2b45
vallentini <vallentinigabrielli.silva@gmail.com>
2025-06-30 15:51:30 -0300

    feat: aula 16: Autenticação e autorização.

--- core/serializers/autor.py
+++ core/serializers/autor.py
@@ -7,3 +7,10 @@ class AutorSerializer(ModelSerializer):
     class Meta:
         model = Autor
         fields = "__all__"
+
+    def __str__(self):
+        return f'({self.id}) {self.nome}'
+
+    class Meta:
+        verbose_name = 'autor'
+        verbose_name_plural = 'autores'
